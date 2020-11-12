c44d35f984d4ca686c25e8b96baff7b2eac134da
Dhruv Joshi <dhruvjoshi03@hotmail.com>
2020-11-13 02:09:38 +0530

    Solved Challenges

---
+++ natas/level15/level15.py
@@ -0,0 +1,18 @@
+import requests
+import string
+
+char = lowercase + uppercase + digits
+
+username = 'natas15'
+password = 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J'
+
+seen_password = list()
+while(True)
+
+
+url = 'http://%s.natas.labs.overthewire.org'  % username
+
+
+response = requests.post(url, "password":"anything" }, auth = (username, password))
+print(response.text)
+ 
